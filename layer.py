415f18cb1887f96e12bea813b2b1fd0142d9865c
Santhosh ps <santhosh.ps@perfomatix.com>
2018-02-18 18:51:51 +0530

    handle media message

--- layer.py
+++ layer.py
@@ -10,40 +10,43 @@ class EchoLayer(YowInterfaceLayer):
     @ProtocolEntityCallback("message")
     def onMessage(self, messageProtocolEntity):
         #send receipt otherwise we keep receiving the same message over and over
-        incomingMsg = messageProtocolEntity.getBody()
-        sender = messageProtocolEntity.getFrom()
-        print(sender+" says "+incomingMsg)
-        if  incomingMsg== "hn" or incomingMsg == "Hn" or incomingMsg == "HN":
-            payload = {'tags':'front_page'}
-            r = requests.get(self.hnApi,params=payload)
-            response = r.json()
-            if r.status_code == 500:
-                msg = response.get('error')
+       if messageProtocolEntity.getType() == 'media':
+          self.sendMsg(messageProtocolEntity,"")
+       else:
+            incomingMsg = messageProtocolEntity.getBody()
+            sender = messageProtocolEntity.getFrom()
+            print(sender+" says "+incomingMsg)
+            if  incomingMsg== "hn" or incomingMsg == "Hn" or incomingMsg == "HN":
+                payload = {'tags':'front_page'}
+                r = requests.get(self.hnApi,params=payload)
+                response = r.json()
+                if r.status_code == 500:
+                    msg = response.get('error')
+                else:
+                    posts = response.get('hits')
+                    msg='*Hacker News For the day* \n\n'
+                    for post in posts:
+                        msg+=''+post.get('title')+'\n'+'*points :* '+str(post.get('points'))+'\n\n Url : '+post.get('url')+'\n ------------------------------------------------- \n\n'
+                self.sendMsg(messageProtocolEntity,msg)
             else:
-                posts = response.get('hits')
-                msg='*Hacker News For the day* \n\n'
-                for post in posts:
-                    msg+=''+post.get('title')+'\n'+'*points :* '+str(post.get('points'))+'\n\n Url : '+post.get('url')+'\n ------------------------------------------------- \n\n'
-            receipt = OutgoingReceiptProtocolEntity(messageProtocolEntity.getId(),messageProtocolEntity.getFrom(), 'read', messageProtocolEntity.getParticipant())
-            outgoingMessageProtocolEntity = TextMessageProtocolEntity(
-                msg,
-               to = messageProtocolEntity.getFrom())
-           
-            self.toLower(receipt)
-            self.toLower(outgoingMessageProtocolEntity)
-     
-        else:
-            receipt = OutgoingReceiptProtocolEntity(messageProtocolEntity.getId(), messageProtocolEntity.getFrom(), 'read', messageProtocolEntity.getParticipant())
-           
-            outgoingMessageProtocolEntity = TextMessageProtocolEntity(
-                messageProtocolEntity.getBody(),
-               to = messageProtocolEntity.getFrom())
-           
-            self.toLower(receipt)
-            self.toLower(outgoingMessageProtocolEntity)
+                self.sendMsg(messageProtocolEntity,"")
+               
 
     @ProtocolEntityCallback("receipt")
     def onReceipt(self, entity):
         ack = OutgoingAckProtocolEntity(entity.getId(), "receipt", entity.getType(), entity.getFrom())
         self.toLower(ack)
  
+    def sendMsg(self,messageProtocolEntity,message):
+        receipt = OutgoingReceiptProtocolEntity(messageProtocolEntity.getId(),
+         messageProtocolEntity.getFrom(), 'read', messageProtocolEntity.getParticipant())
+        if message=="":
+            outgoingMessageProtocolEntity = TextMessageProtocolEntity(
+                    "Welcome to hacker news bot, try sending 'hn' for *hacker-news*",
+                to = messageProtocolEntity.getFrom())
+        else:
+            outgoingMessageProtocolEntity = TextMessageProtocolEntity(
+                    message,
+                to = messageProtocolEntity.getFrom())
+        self.toLower(receipt)
+        self.toLower(outgoingMessageProtocolEntity)
