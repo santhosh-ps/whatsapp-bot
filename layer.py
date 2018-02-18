af7069fa3072e33c9b78219e54f2e0bb3c7dcefd
Santhosh ps <santhosh.ps@perfomatix.com>
2018-02-18 14:56:06 +0530

    whatsapp bot to get hn news for the day

---
+++ layer.py
@@ -0,0 +1,49 @@
+from yowsup.layers.interface                           import YowInterfaceLayer, ProtocolEntityCallback
+from yowsup.layers.protocol_messages.protocolentities  import TextMessageProtocolEntity
+from yowsup.layers.protocol_receipts.protocolentities  import OutgoingReceiptProtocolEntity
+from yowsup.layers.protocol_acks.protocolentities      import OutgoingAckProtocolEntity
+
+import requests 
+class EchoLayer(YowInterfaceLayer):
+    hnApi = "http://hn.algolia.com/api/v1/search"
+
+    @ProtocolEntityCallback("message")
+    def onMessage(self, messageProtocolEntity):
+        #send receipt otherwise we keep receiving the same message over and over
+        incomingMsg = messageProtocolEntity.getBody()
+        sender = messageProtocolEntity.getFrom()
+        print(sender+" says "+incomingMsg)
+        if  incomingMsg== "hn" or incomingMsg == "Hn" or incomingMsg == "HN":
+            payload = {'tags':'front_page'}
+            r = requests.get(self.hnApi,params=payload)
+            response = r.json()
+            if r.status_code == 500:
+                msg = response.get('error')
+            else:
+                posts = response.get('hits')
+                msg='*Hacker News For the day* \n\n'
+                for post in posts:
+                    msg+=''+post.get('title')+'\n'+'points : '+str(post.get('points'))+'\n\n Url : '+post.get('url')+'\n ------------------------------------------------- \n\n'
+            receipt = OutgoingReceiptProtocolEntity(messageProtocolEntity.getId(),messageProtocolEntity.getFrom(), 'read', messageProtocolEntity.getParticipant())
+            outgoingMessageProtocolEntity = TextMessageProtocolEntity(
+                msg,
+               to = messageProtocolEntity.getFrom())
+           
+            self.toLower(receipt)
+            self.toLower(outgoingMessageProtocolEntity)
+     
+        else:
+            receipt = OutgoingReceiptProtocolEntity(messageProtocolEntity.getId(), messageProtocolEntity.getFrom(), 'read', messageProtocolEntity.getParticipant())
+           
+            outgoingMessageProtocolEntity = TextMessageProtocolEntity(
+                messageProtocolEntity.getBody(),
+               to = messageProtocolEntity.getFrom())
+           
+            self.toLower(receipt)
+            self.toLower(outgoingMessageProtocolEntity)
+
+    @ProtocolEntityCallback("receipt")
+    def onReceipt(self, entity):
+        ack = OutgoingAckProtocolEntity(entity.getId(), "receipt", entity.getType(), entity.getFrom())
+        self.toLower(ack)
+ 
---
+++ run.py
@@ -0,0 +1,35 @@
+from yowsup.stacks import  YowStackBuilder
+from layer import EchoLayer
+from yowsup.layers.auth import AuthError
+from yowsup.layers import YowLayerEvent
+from yowsup.layers.network import YowNetworkLayer
+from yowsup.env import YowsupEnv
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+envPath = str(Path.home()) + '/whatsappbot.env'
+print(envPath)
+load_dotenv(envPath)
+
+
+UNAME = str(os.environ.get("uname"))
+PASS = str(os.environ.get("pass"))
+
+credentials = (UNAME, PASS) # replace with your phone and password
+
+if __name__==  "__main__":
+    stackBuilder = YowStackBuilder()
+
+    stack = stackBuilder\
+        .pushDefaultLayers(True)\
+        .push(EchoLayer)\
+        .build()
+    print("setting credentials")
+    stack.setCredentials(credentials)
+    stack.broadcastEvent(YowLayerEvent(YowNetworkLayer.EVENT_STATE_CONNECT))   #sending the connect signal
+    try:
+        stack.loop() #this is the program mainloop
+    except AuthError as e:
+        print("Authentication Error: %s" % e.message)
+    
